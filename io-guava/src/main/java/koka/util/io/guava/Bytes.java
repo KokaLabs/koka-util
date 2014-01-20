13e149e703d8007a55c55c895550bffd3d8a8930
Scotty Tomaszewski <scottTomaszewski@gmail.com>
2014-01-20 10:16:38 -0500

    Replaces InputSupplier with ByteSource

--- io-guava/src/main/java/koka/util/io/guava/Bytes.java
+++ io-guava/src/main/java/koka/util/io/guava/Bytes.java
@@ -1,7 +1,6 @@
 package koka.util.io.guava;
 
 import static com.google.common.base.Charsets.UTF_8;
-import koka.util.io.guava.PipedFromOutput;
 
 import java.io.FilterOutputStream;
 import java.io.IOException;
@@ -12,18 +11,16 @@ import java.util.Random;
 
 import com.google.common.annotations.Beta;
 import com.google.common.io.BaseEncoding;
-import com.google.common.io.ByteStreams;
+import com.google.common.io.ByteSource;
 import com.google.common.io.Closer;
 import com.google.common.io.FileBackedOutputStream;
-import com.google.common.io.InputSupplier;
-import com.google.common.io.OutputSupplier;
 
 /**
  * Assists in generating arbitrary random data in a useful format for tests in
  * an entirely streaming way.
  */
 @Beta
-public abstract class Bytes implements InputSupplier<InputStream> {
+public abstract class Bytes extends ByteSource {
   private static final long SEED = 8L;
 
   public static Unsized random() {
@@ -40,7 +37,7 @@ public abstract class Bytes implements InputSupplier<InputStream> {
       }
 
       @Override
-      public InputStream getInput() {
+      public InputStream openStream() {
         return new InputStream() {
           private final Random byteGenerator = new Random(seed);
 
@@ -68,20 +65,20 @@ public abstract class Bytes implements InputSupplier<InputStream> {
     return new EndlessRandom();
   }
 
-  public static Bytes of(final InputSupplier<? extends InputStream> raw) {
+  public static Bytes of(final ByteSource raw) {
     return new Bytes() {
       @Override
-      public InputStream getInput() throws IOException {
-        return raw.getInput();
+      public InputStream openStream() throws IOException {
+        return raw.openStream();
       }
     };
   }
 
   public final Bytes limitedTo(long length) {
-    return of(ByteStreams.slice(this, 0, length));
+    return of(this.slice(0, length));
   }
 
-  // Uncomment for optimization if commons-io is ever added 
+  // Uncomment for optimization if commons-io is ever added
 //  /**
 //   * faster implementation
 //   */
@@ -101,62 +98,51 @@ public abstract class Bytes implements InputSupplier<InputStream> {
 //      return this.inBase64();
 //    }
     final Bytes curr = this;
-    return of(new PipedFromOutput() {
+    return of(new ByteSource() {
       @Override
-      protected void write(OutputStream to) throws IOException {
-        OutputStream ignored = new FilterOutputStream(to) {
+      public InputStream openStream() throws IOException {
+        return new PipedFromOutput() {
           @Override
-          public void write(byte[] b) throws IOException {
-            out.write(b);
-          }
+          protected void write(OutputStream to) throws IOException {
+            OutputStream ignored = new FilterOutputStream(to) {
+              @Override
+              public void write(byte[] b) throws IOException {
+                out.write(b);
+              }
 
-          @Override
-          public void write(byte[] b, int off, int len) throws IOException {
-            out.write(b, off, len);
-          }
+              @Override
+              public void write(byte[] b, int off, int len) throws IOException {
+                out.write(b, off, len);
+              }
 
-          @Override
-          public void write(int b) throws IOException {
-            out.write(b);
-          }
+              @Override
+              public void write(int b) throws IOException {
+                out.write(b);
+              }
 
-          @Override
-          public void close() {}
-        };
-        OutputStreamWriter out = new OutputStreamWriter(ignored, UTF_8);
-        Closer c = Closer.create();
-        try {
-          ByteStreams.copy(curr, c.register(as.encodingStream(out)));
-        } catch (Throwable e) {
-          throw c.rethrow(e);
-        } finally {
-          c.close();
-        }
+              @Override
+              public void close() {}
+            };
+            OutputStreamWriter out = new OutputStreamWriter(ignored, UTF_8);
+            Closer c = Closer.create();
+            try {
+              curr.copyTo(c.register(as.encodingStream(out)));
+            } catch (Throwable e) {
+              throw c.rethrow(e);
+            } finally {
+              c.close();
+            }
+          }
+        }.getInput();
       }
     });
-  }
-
-  public final void copyTo(OutputSupplier<? extends OutputStream> to)
-      throws IOException {
-    Closer c = Closer.create();
-    try {
-      copyTo(c.register(to.getOutput()));
-    } catch (Throwable e) {
-      throw c.rethrow(e);
-    } finally {
-      c.close();
-    }
-  }
-
-  public void copyTo(OutputStream to) throws IOException {
-    ByteStreams.copy(this, to);
   }
 
   @Beta
   public final Bytes cacheToDisk() throws IOException {
     FileBackedOutputStream cache = new FileBackedOutputStream(1_000_000);
-    ByteStreams.copy(this, cache);
-    return Bytes.of(cache.getSupplier());
+    this.copyTo(cache);
+    return Bytes.of(cache.asByteSource());
   }
   
   private Bytes() {}
