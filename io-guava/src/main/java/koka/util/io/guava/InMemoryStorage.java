bbbd1ddb27696e60f573fc3ef76c11da8a0178b3
Scotty Tomaszewski <scottTomaszewski@gmail.com>
2014-01-20 12:40:16 -0500

    Updates comment with deprecated info

--- io-guava/src/main/java/koka/util/io/guava/InMemoryStorage.java
+++ io-guava/src/main/java/koka/util/io/guava/InMemoryStorage.java
@@ -5,12 +5,21 @@ import java.io.OutputStream;
 
 import koka.util.io.OutToInStream;
 
+import com.google.common.io.ByteSink;
+import com.google.common.io.ByteSource;
+import com.google.common.io.InputSupplier;
+
 /**
  * Stores data in memory.
  * <p>
  * Note: multiple writes will append. Create a new instance if intending to
  * clear the buffer.
+ * 
+ * @deprecated Replaced by {@link InMemoryBytes} using {@link ByteSource} and
+ *             {@link ByteSink}. To be To be removed once Guava removes
+ *             {@link InputSupplier} in version 18.0
  */
+@Deprecated
 public final class InMemoryStorage implements Storage {
   private final OutToInStream outToIn = new OutToInStream();
 
